4fa73aa2697e79a1ed9f69ec528f4749e196b6bc
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2022-03-20 01:30:01 +0530

    Initial Layout

--- lib/main.dart
+++ lib/main.dart
@@ -28,10 +28,69 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
+
+  Widget x({color,num}){
+    return SizedBox(
+      height: 400,
+      width: 400,
+      child: Container(
+        color: color,
+
+        child: ElevatedButton(
+          child: Text(num),
+          onPressed: (){},
+        ),
+      ),
+    );
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // appBar: AppBar(),
+      backgroundColor: Colors.white,
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10.0, 35.0, 10.0, 10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(flex:4,child: Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                Container(
+                  height: 200,
+                  color: Colors.greenAccent,
+                ),
+                const Text('1+1=0',style: TextStyle(fontSize: 60.0),),
+              ],
 
+            )),
+            Expanded(flex:8,child:GridView.count(
+              crossAxisCount: 4,
+              children: [
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.red,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.blue,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.blue,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.blue,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.red,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.blue,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.blue,num:2.toString()),
+                x(color: Colors.amber,num:1.toString()),
+                x(color: Colors.blue,num:2.toString()),
+              ],
+            ),),
+          ],
+        ),
+      ),
     );
   }
 }
