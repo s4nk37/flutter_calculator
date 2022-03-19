10ef804ab4c9756f439b2b4aa14963ae91c7abe5
Sanket Patel <44511437+s4nk37@users.noreply.github.com>
2022-03-20 02:43:30 +0530

    UI Building

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Color(0xffe5e5e5),
-        backgroundColor: Colors.black,
+        brightness: Brightness.light,
+        primaryColor: const Color(0xffe5e5e5),
       ),
       title: 'Calculator',
       home: Calculator(),
@@ -28,66 +27,89 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-
-  Widget x({color,num}){
-    return SizedBox(
-      height: 400,
-      width: 400,
-      child: Container(
-        color: color,
-
-        child: ElevatedButton(
-          child: Text(num),
-          onPressed: (){},
+  Widget myButton({color, text}) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        primary: color,
+        shadowColor: Colors.transparent,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100.0),
         ),
       ),
+      child: Text(
+        text.toString(),
+        style: TextStyle(fontSize: 30.0),
+      ),
+      onPressed: () {},
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(),
-      backgroundColor: Colors.white,
+      backgroundColor: const Color(0xffe5e5e5),
       body: Container(
         padding: EdgeInsets.fromLTRB(10.0, 35.0, 10.0, 10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Expanded(flex:4,child: Stack(
-              alignment: Alignment.bottomRight,
-              children: [
-                Container(
-                  height: 200,
-                  color: Colors.greenAccent,
-                ),
-                const Text('1+1=0',style: TextStyle(fontSize: 60.0),),
-              ],
-
-            )),
-            Expanded(flex:8,child:GridView.count(
-              crossAxisCount: 4,
-              children: [
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.red,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.blue,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.blue,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.blue,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.red,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.blue,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.blue,num:2.toString()),
-                x(color: Colors.amber,num:1.toString()),
-                x(color: Colors.blue,num:2.toString()),
-              ],
-            ),),
+            Expanded(
+                flex: 6,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Stack(
+                    alignment: Alignment.bottomRight,
+                    children: [
+                      Container(
+                        height: 200,
+                        color: Colors.transparent,
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            '1+1',
+                            style:
+                                TextStyle(fontSize: 45.0, color: Colors.grey),
+                          ),
+                          Text(
+                            '=0',
+                            style: TextStyle(
+                                fontSize: 65.0, color: Colors.black54),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                )),
+            Expanded(
+              flex: 8,
+              child: GridView.count(
+                crossAxisCount: 4,
+                crossAxisSpacing: 7.0,
+                mainAxisSpacing: 7.0,
+                children: [
+                  myButton(color: Colors.greenAccent, text: 1),
+                  myButton(color: Colors.greenAccent, text: 2),
+                  myButton(color: Colors.greenAccent, text: 3),
+                  myButton(color: Colors.greenAccent, text: 4),
+                  myButton(color: Colors.greenAccent, text: 5),
+                  myButton(color: Colors.greenAccent, text: 6),
+                  myButton(color: Colors.greenAccent, text: 7),
+                  myButton(color: Colors.greenAccent, text: 8),
+                  myButton(color: Colors.greenAccent, text: 9),
+                  myButton(color: Colors.greenAccent, text: 10),
+                  myButton(color: Colors.greenAccent, text: 11),
+                  myButton(color: Colors.greenAccent, text: 12),
+                  myButton(color: Colors.greenAccent, text: 13),
+                  myButton(color: Colors.greenAccent, text: 14),
+                  myButton(color: Colors.greenAccent, text: 15),
+                  myButton(color: Colors.greenAccent, text: 16),
+                ],
+              ),
+            ),
           ],
         ),
       ),
